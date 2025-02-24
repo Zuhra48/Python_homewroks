ad8b876537b51eb9da3c8bcaf90fe4801d1d9474
Zuhra48 <zuhra83300@gmail.com>
2025-02-24 12:22:53 +0500

    Update dictionary.py

--- lesson-3/homework/dictionary.py
+++ lesson-3/homework/dictionary.py
@@ -1,1 +1,9 @@
-
+def count_vowels_in_sentence(sentence):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in sentence:
+        if char in vowels:
+            count +=1
+    return count
+sentence = input("Enter a sentence:")
+print("number of vowels:",count_vowels_in_sentence(sentence)) 
