5c1aa991a6062234c922e0de93f76c84f6192c8e
Zuhra48 <zuhra83300@gmail.com>
2025-02-24 12:47:30 +0500

    Delete lesson-3/homework/dictionary.py

--- lesson-3/homework/dictionary.py
+++
@@ -1,9 +0,0 @@
-def count_vowels_in_sentence(sentence):
-    vowels = "aeiouAEIOU"
-    count = 0
-    for char in sentence:
-        if char in vowels:
-            count +=1
-    return count
-sentence = input("Enter a sentence:")
-print("number of vowels:",count_vowels_in_sentence(sentence)) 
