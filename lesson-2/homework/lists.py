61f2328ce41afae09ff6e46d36e204415d49510b
Zuhra48 <zuhra83300@gmail.com>
2025-02-24 12:34:54 +0500

    Update lists.py

--- lesson-2/homework/lists.py
+++ lesson-2/homework/lists.py
@@ -1,1 +1,39 @@
+# task 1
 
+a = "how old are you"
+print(len(a))
+
+#task 2 
+
+country = 'Uzbekistan'
+print(country[:2]) + (country[-2:])
+
+#task 3
+
+name = 'Sakina'
+name2 = name.replace('n','$')
+name2
+
+# task 4
+
+def belgilar_mix(a,b):
+    al = b[:2] + a[2:]
+    bl = a[:2] + b[2:]
+
+    return al + ' ' + bl
+print(belgilar_mix('Zuhra','Hasan'))
+
+# task 7
+
+a = 'how old are you'
+print(len(a))
+
+# task 8
+
+a = 'how old are you'
+print(len(a))
+
+# Task 10
+
+city = 'Tashkent'
+print(city[:4])
